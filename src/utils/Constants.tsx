be105019cec3cb4faa8a9a924cfd188bbb48df84
John Ítalo Lucas Lima <jhon2italo@gmail.com>
2022-10-22 17:24:22 -0300

    teste em produção

--- src/utils/Constants.tsx
+++ src/utils/Constants.tsx
@@ -1,3 +1,4 @@
 export const URL = "https://api.themoviedb.org/3/";
 export const QUERYS = "?api_key=617375c16cb7cbacc59f9c2b6102e4e4&language=pt-BR&page=1";
 export const URL_IMG = "https://image.tmdb.org/t/p/w300";
+export const URLd_IMG = "https://image.tmdb.org/t/p/w300";
